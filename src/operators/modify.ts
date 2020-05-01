d577bbc2bb8d63a0a6fad2f69fce5b634d4d10c2
Matt Wilson <matt@matt-wilson.org>
2020-05-01 14:13:15 +0100

    refactor: refactor operators to put the implementation into the class definition (all operators must implement an action method)

--- src/operators/modify.ts
+++ src/operators/modify.ts
@@ -1,23 +1,24 @@
-// import { Draft, produce } from 'immer';
 import { Draft, produce } from 'immer';
 
 import { Limiter } from '../limiter';
-import { Operator, OperatorActionFunction } from '../operator';
-
-class ModifyOperator<T> extends Operator<T> {
-  constructor(action: OperatorActionFunction<T>) {
-    super('modify', action);
-  }
-}
+import { Operator } from '../operator';
 
 type ModifierFunction<T> = (itemDraft: Draft<T>, index: number) => void;
 
-export function modify<T>(modifierFunction: ModifierFunction<T>): ModifyOperator<T> {
-  return new ModifyOperator((items: T[], limiter: Limiter<T>) => {
+class ModifyOperator<T> extends Operator<T> {
+  constructor(private modifierFunction: ModifierFunction<T>) {
+    super('modify');
+  }
+
+  action(items: T[], limiter: Limiter<T>): T[] {
     return items.map((item, index) => {
       if (!limiter(item, index)) { return item; }
       
-      return produce(item, draft => { modifierFunction(draft, index) });
+      return produce(item, draft => { this.modifierFunction(draft, index) });
     });
-  });
+  }
+}
+
+export function modify<T>(modifierFunction: ModifierFunction<T>): ModifyOperator<T> {
+  return new ModifyOperator(modifierFunction);
 }
